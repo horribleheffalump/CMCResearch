45c68b39d7639eac8f1b982b26c222ca9a100a48
gregbmiller <gregbmiller@gmail.com>
2016-08-03 13:13:06 +0300

    first attempt to commit

---
+++ CMCTools/MCIntensityMatrix/MCIntensityMatrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace MCIntensityMatrix
+{
+    public class MCIntensityMatrix : Matrix<double>
+    {
+    }
+}
