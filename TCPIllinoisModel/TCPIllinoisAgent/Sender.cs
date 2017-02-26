f53743225dda093babcef816164ab51a00235878
gmiller <gregbmiller@gmail.com>
2017-02-26 19:53:42 +0300

    Slooooow start

---
+++ TCPIllinoisModel/TCPIllinoisAgent/Sender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPIllinoisAgent
+{
+    public class Sender
+    {
+        int W0 = 1; //min windiw size
+        int W1;     //slow start -> congestion avoidance threshold
+        //double
+    }
+}
