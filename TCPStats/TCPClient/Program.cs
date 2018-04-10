db954f3046f5fe04341368a52240b785620e9437
Gregory Miller <gregbmiller@gmail.com>
2018-04-10 15:53:45 +0300

    basic sender and listener

---
+++ TCPStats/TCPClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //DoSendFile("127.0.0.1", @"..\..\tosend.txt", 10000);
+            DoSendFile(args[0], args[1], int.Parse(args[2]));
+        }
+
+        private static async void DoSendFile(string server, string filename, int times = 1)
+        {
+            Console.WriteLine("start sending");
+            Console.WriteLine("press any key to stop");
+            using (CancellationTokenSource ct = new CancellationTokenSource())
+            {
+                Task s = SendFile(server, filename, times, ct.Token);
+                Console.ReadKey();
+                Console.WriteLine("cancelled");
+                ct.Cancel();
+                await s;
+                Console.ReadKey();
+            }
+        }
+
+        private static async Task SendFile(string server, string filename, int times, CancellationToken ct)
+        {
+            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                for (int i = 0; i < times; i++)
+                {
+                    if (ct.IsCancellationRequested)
+                        break;
+                    else
+                    {
+                        fs.Position = 0;
+                        await Send(server, fs, ct);
+                        Console.WriteLine($"{i + 1}/{times} file sent");
+                    }
+                }
+                Console.WriteLine("finished");
+            }
+        }
+
+        private static async Task Send(string server, Stream stream, CancellationToken ct)
+        {
+            Int32 port = 13000;
+            using (TcpClient client = new TcpClient(server, port))
+            using (NetworkStream networkStream = client.GetStream())
+            {
+                Task doCopy = stream.CopyToAsync(networkStream, (int)stream.Length, ct);
+                await doCopy;
+            }
+        }
+    }
+}
--- TCPStats/TCPListener/Program.cs
+++ TCPStats/TCPListener/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
 
 namespace TCPListener
 {
@@ -10,6 +10,82 @@ namespace TCPListener
     {
         static void Main(string[] args)
         {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                server = new TcpListener(port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                long bytes_received = 0;
+                int connections = 0;
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+                    connections++;
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+                    long bytes_received_chunc = 0;
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        bytes_received += data.Length;
+                        bytes_received_chunc += data.Length;
+                        if (bytes_received_chunc > 1e6)
+                        {
+                            Console.WriteLine($"received {bytes_received} bytes");
+                            bytes_received_chunc = 0;
+                        }
+                        //Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        //data = data.ToUpper();
+
+                        //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        //// Send back a response.
+                        //stream.Write(msg, 0, msg.Length);
+                        //Console.WriteLine("Sent: {0}", data);
+                    }
+                    Console.WriteLine($"Total {bytes_received} bytes ({bytes_received / 1000.0 / 1000.0} Mb) in {connections} files");
+
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
         }
     }
 }
